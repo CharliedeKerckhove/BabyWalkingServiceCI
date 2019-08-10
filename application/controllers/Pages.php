ede4317824f1e458b8b0e08fa49560b6e4e525c7
CrazyC008 <DEKC1_16@uni.worc.ac.uk>
2019-08-10 18:28:28 +0100

    if statement to load correct login files

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -3,13 +3,25 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Pages extends CI_Controller {
 
-	public function view($page = 'home'){
+	public function view($page = 'home', $status = 'pages/'){
         if(!file_exists(APPPATH.'views/pages/' . $page . '.php')){
             show_404();
         }
-        
-		$this->load->view('includes/header');
-		$this->load->view('pages/' . $page);
-		$this->load->view('includes/footer');
+        //log in status
+        if ($status === 'pages/') {
+            $this->load->view('includes/header');
+            $this->load->view($status . $page);
+            $this->load->view('includes/footer');
+        } else if ($status === 'carer/') {
+            $this->load->view('includes/carerheader');
+            $this->load->view($status . $page);
+            $this->load->view('includes/carerfooter');
+        } else if ($status === 'admin/') {
+            $this->load->view('includes/adminheader');
+            $this->load->view($status . $page);
+            $this->load->view('includes/adminfooter');
+        } else {
+            show_404();
+        }
 	}
 }
